4b66c961174893d5b262ed3271370c1eb4055bbb
Rishabh Kumar <rishabhkumar140999@gmail.com>
2025-03-01 20:27:53 -0500

    Chat Window

--- src/layouts/upload-image/index.js
+++ src/layouts/upload-image/index.js
@@ -31,6 +31,7 @@ import PaymentMethod from "layouts/billing/components/PaymentMethod";
 import Invoices from "layouts/billing/components/Invoices";
 import BillingInformation from "layouts/billing/components/BillingInformation";
 import Transactions from "layouts/billing/components/Transactions";
+import SJSDiagnosisSection from "layouts/upload-image/components/SJSDiagnosisSection";
 
 function Billing() {
   return (
@@ -40,31 +41,9 @@ function Billing() {
         <MDBox mb={3}>
           <Grid container spacing={3}>
             {/* Rishabh */}
+
             <Grid item xs={12} lg={8}>
-              <Grid container spacing={3}>
-                <Grid item xs={12} xl={6}>
-                  <MasterCard number={4562112245947852} holder="jack peterson" expires="11/22" />
-                </Grid>
-                <Grid item xs={12} md={6} xl={3}>
-                  <DefaultInfoCard
-                    icon="account_balance"
-                    title="salary"
-                    description="Belong Interactive"
-                    value="+$2000"
-                  />
-                </Grid>
-                <Grid item xs={12} md={6} xl={3}>
-                  <DefaultInfoCard
-                    icon="paypal"
-                    title="paypal"
-                    description="Freelance Payment"
-                    value="$455.00"
-                  />
-                </Grid>
-                <Grid item xs={12}>
-                  <PaymentMethod />
-                </Grid>
-              </Grid>
+              <SJSDiagnosisSection />
             </Grid>
 
             {/* Qiming */}
